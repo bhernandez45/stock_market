47baa6ae5e3ed316b211ad6dae5d955d688e2bf7
Brittney Hernandez <brittneyhernandez@Brittneys-MacBook-Pro.local>
2020-07-18 15:24:13 -0400

    adding script file

---
+++ growth_model.R
@@ -0,0 +1,18 @@
+install.packages("lme4")
+library(lme4)
+
+
+corr(finance_data)
+
+# reccodes and cleaning
+#finance_data$time=
+#finance_data$success[finance_data$close>0]=1
+#finance_data$success[finance_data$close<0]=0
+
+
+# unconditional growth model
+m0=lmer(success~ 1 + (1|time), data=finance_data, family="binomial")
+
+m1=lmer(success~1 + (1|time), data=finance_data, family="binomial")
+
+icc_mod_fin=
